d5d2bb4d1353be69a8d62ae70a98ce2fb94479de
Rasmus Løvstad <rasmus.hag.lovstad@gmail.com>
2017-09-23 18:19:39 +0200

    Heavily improved front page student list

--- gruppeapp/static/gruppeapp/js/welcome.js
+++ gruppeapp/static/gruppeapp/js/welcome.js
@@ -2,18 +2,44 @@ var counter = 0;
 var studentid = counter+1;
 $("document").ready(function() {
   $("#addstudent").submit(function() {
+
     var currentStudent = $("#nameinput").val();
-    $("#nameinput").val("");
-    $("#grouptable").append("<tr class='student'><td>"+ studentid +"</td><td>"+currentStudent+"</td></tr>");
-    $("#studentlist").append("<input class='student' type='hidden' value="+currentStudent+" name=student"+counter+">");
-    $("#studentcounter").val(counter);
-    counter++;
-    studentid = counter+1;
-  });
-  $("#clear").click(function() {
-    $(".student").remove();
-    counter = 0;
-    $("#studentcounter").val(counter);
+    if (currentStudent !== "") {
+        $("#nameinput").val("");
+        $("#grouplist").append("<li class='list-group-item' id='container"+counter+"'>"+
+        "<div class='row'>"+
+        "<div class='col-md-8'>"+
+                      "<p  data-type='text' data-title='Enter student name' class='studentnamefield'>"+currentStudent+"</p>"+
+                    "</div>"+
+                    "<div class='col-md-4 pull-right'>"+
+                      "<span class='glyphicon glyphicon-remove removestudent pull-right' data-student="+counter+"></span> <br>"+
+                    "</div>"+
+                    "</div>"+
+                    "</li>");
+
+        $("#studentlist").append("<input class='student' type='hidden' value="+currentStudent+" name='student"+counter+"' id='student"+counter+"'>");
+
+        counter++;
+        studentid = counter+1;
+        $("#studentcounter").val(counter);
+        }
+      });
+      $("#clear").click(function() {
+        $(".student").remove();
+        counter = 0;
+        $("#studentcounter").val(counter);
+      });
+
+      $("#grouplist").on("click", ".removestudent", function(event) {
+      console.log("Remove "+ $(event.currentTarget).data("student"));
+        var string = $(event.currentTarget).data("student");
+        $("#container"+string).remove();
+        $("#student"+string).remove();
+        $("#studentcounter").val(counter);
+        counter--;
+
+        studentid = counter+1;
+
   });
 
 
@@ -21,6 +47,7 @@ $("document").ready(function() {
   //counter = $("input:checked").length;
   //$("#studentsfromclasscounter").val(counter);
   return true;
+
   });
 
 
